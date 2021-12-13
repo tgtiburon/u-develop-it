a1bb3cc55ee74b32f3392c80b8d04d1ed8ac3f3c
tgtiburon <tg.tiburon@gmail.com>
2021-12-13 15:35:33 -0500

    all db queries set up

--- server.js
+++ server.js
@@ -29,10 +29,45 @@ app.get('/', (req, res) => {
     });
 });
 
+// // Lists all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows)=> {
+//     console.log(rows);
+// });
 
-db.query(`SELECT * FROM candidates`, (err, rows)=> {
-    console.log(rows);
-});
+
+
+// // Gets a single candidate by their id
+// db.query(`SELECT * FROM candidates WHERE id =1`, (err, row)=> {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+
+// Query for delete operation
+// ? denotes a placeholder, making this a prepared statement.
+// can execute the same statement with different values
+// // 1 goes into the ? spot
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// // params has to match VALUES
+// const params = [1,'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result)=> {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 //Default response for any other request (not found)   
 // this has to be below all valid gets or it will block them
