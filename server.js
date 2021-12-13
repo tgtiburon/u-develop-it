76b43ba3ea65c7ec9733e2ae09142a2799448101
tgtiburon <tg.tiburon@gmail.com>
2021-12-13 17:16:56 -0500

    used JOIN to tie two db together

--- server.js
+++ server.js
@@ -34,7 +34,14 @@ app.get('/', (req, res) => {
 // Get all candidates
 app.get('/api/candidates', (req, res)=> {
 
-    const sql = `SELECT * FROM candidates`;
+    // Was
+    //const sql = `SELECT * FROM candidates`;
+
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     // Lists all candidates
     db.query(sql, (err, rows)=> {
@@ -56,7 +63,15 @@ app.get('/api/candidates', (req, res)=> {
 
 // Gets a single candidate by their id
 app.get('/api/candidate/:id', (req, res)=> {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    //was
+   // const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
+                
     const params = [req.params.id];
 
 
